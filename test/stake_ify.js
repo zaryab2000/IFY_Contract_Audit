e5752b66fdb3605753030b811172da8b2eb4ed87
zaryab2000 <zaryabafser2000@gmail.com>
2021-01-27 01:16:47 +0530

    updated Stake test scripts

--- test/stake_ify.js
+++ test/stake_ify.js
@@ -34,22 +34,24 @@ contract("IFY Staking Contract Test", accounts => {
 
   it("Transfer sufficient tokens to 3 users and Staking contract",async()=>{
   	const tokensToTransfer = new BN(50000000000000000000000)
-  	const tokensToRecieve = new BN(47500000000000000000000);
+  	const tokensToTransferStake = new BN(50000000000000000000000)
+
+    const tokensToRecieve = new BN(47500000000000000000000);
   	await tokenInstance.transfer(accounts[1],tokensToTransfer);
   	await tokenInstance.transfer(accounts[2],tokensToTransfer);
   	await tokenInstance.transfer(accounts[3],tokensToTransfer);
   
-  	await tokenInstance.transfer(stakeInstance.address,tokensToTransfer);
+  	await tokenInstance.transfer(stakeInstance.address,tokensToTransferStake);
   	await tokenInstance.approve(stakeInstance.address,tokensToRecieve,{from:accounts[1]});
   	await tokenInstance.approve(stakeInstance.address,tokensToRecieve,{from:accounts[2]});
   	await tokenInstance.approve(stakeInstance.address,tokensToRecieve,{from:accounts[3]});
 	
   })
   /** Stake function checkpoints:
-   * Should only work if optionNumber is between 1 to 4
-   * Should only work if stakedAmount is equal to 0
-   * stakedAmount,stakingOpt,endingDate should be assigned correctly
-   * Check the reward amount is right or incorrect
+   * Should only work if optionNumber is between 1 to 4 -done
+   * Should only work if stakedAmount is equal to 0 -done
+   * stakedAmount,stakingOpt,endingDate should be assigned correctly -done
+   * Check the reward amount is right or incorrect -done
    * transfer of token from user to contract address is done prpoerly
   */
   it("Stake function should work as expected for User 1", async () => {
@@ -92,26 +94,32 @@ contract("IFY Staking Contract Test", accounts => {
   });
 
    it("User should not be able to call with invalid option number", async () => {
-
-    // try {
-    // } catch (error) {
-    //   const invalidOpcode = error.message.search("revert") >= 0;
-    //   assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
-    // }
+      const stakeAmount = new BN(10000000000000000000000);
+    try {
+          await stakeInstance.STAKE(stakeAmount,5,{from:accounts[3]});
+          await stakeInstance.STAKE(stakeAmount,0,{from:accounts[3]});
+    } catch (error) {
+      const invalidOpcode = error.message.search("revert") >= 0;
+      console.log(error.message)
+      assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
+    }
   });
 
 
   it("User should not be able to call STAKE function with already staked amount", async () => {
+    const stakeAmount = new BN(10000000000000000000000);
 
-    // try {
-    // } catch (error) {
-    //   const invalidOpcode = error.message.search("revert") >= 0;
-    //   assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
-    // }
+    try{
+         await stakeInstance.STAKE(stakeAmount,3,{from:accounts[1]});
+    } catch (error) {
+      const invalidOpcode = error.message.search("revert") >= 0;
+      console.log(error.message)
+      assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
+    }
   });
-   /** PendingReward function checkpoints:
-    * Shoudl return the reward
-  */
+  /** PendingReward function checkpoints:
+    * Should return the reward
+    */
   it("PendingReward function should work as expected", async () => {
     const rewardClaimed = await stakeInstance.stakers(accounts[2]);
     const reward = await stakeInstance.pendingReward(accounts[2]);
@@ -119,21 +127,32 @@ contract("IFY Staking Contract Test", accounts => {
     assert.equal(rewardClaimed[1].toString(),"0","reward claim is not zero");
     assert.equal(reward.toString(),"24500000000000000000000","Reward is not assigned correctly")
   });
+
   /** ClaimReward function checkpoints:
-    * Should work if pending reward > 0
+    * Should work if pending reward > 0 -done
     * should work after stakingEndTime is over. - done
     * Should only be called after deadline is over - done
-    * Balance before should be small and after shoud be big - done
+    * Balance before should be small and after shoud be increased - done
     * totalClaimed rewards should update - done
     * staker's claim reward should update - done
     * Staking contract balance should reduce- done
-    * Tax should not be applicable
-  */
+    * Tax should not be applicable -done
+    */
 
+   it("User should not be able to call ClaimReward function before EndTime", async () => {
+    try{
+      await stakeInstance.ClaimReward({from:accounts[1]});
+    } catch (error) {
+      const invalidOpcode = error.message.search("revert") >= 0;
+      console.log(error.message)
+      assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
+    }
+  });
 
   it("Time should increase to 30 days", async () => {
     await time.increase(time.duration.seconds(2678400));
   });
+
   it("ClaimReward function should work as expected for User1", async () => {
       const ownerBalanceBefore = await tokenInstance.balanceOf(accounts[0]);
       const balanceBefore = await tokenInstance.balanceOf(accounts[1]);
@@ -147,9 +166,6 @@ contract("IFY Staking Contract Test", accounts => {
       const totalClaimed = await stakeInstance.totalClaimedRewards();
       const staker = await stakeInstance.stakers(accounts[1]);
 
-      // console.log(StakeContractBalance.toString())
-      // console.log(balanceAfter.toString())
-
       assert.equal(ownerBalanceBefore.toString(),ownerBalanceAfter.toString(),"Owner has received some unnecessary tax")
       assert.equal(StakeContractBalance.toString(),"74250000000000000000000","Stake contract Balance before is wrong")
       assert.equal(balanceBefore.toString(),"37500000000000000000000","Balance before is wrong")
@@ -159,20 +175,65 @@ contract("IFY Staking Contract Test", accounts => {
  
   });
 
+
+  it("User should not be able to call ClaimReward function Twice", async () => {
+    try{
+      await stakeInstance.ClaimReward({from:accounts[1]});
+    } catch (error) {
+      const invalidOpcode = error.message.search("revert") >= 0;
+      console.log(error.message)
+      assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
+    }
+  });
   /** UnStake function checkpoints:
-    * Should work if stakedAmount is greater than Zero
-    * should work after stakingEndTime is over
-    * Total staked amount should be updated
-    * Check if pendingReward exist 
+    * Should work if stakedAmount is greater than Zero -done
+    * should work after stakingEndTime is over -done
+    * Total staked amount should be updated -done
+    * Check if pendingReward exist  -done
     * Pending reward must be paid after execution
     * Staked amount should be updatd to ZERO
     * Transfer of function should take place properly
     * Tax should not be applicable
   */
 
+  it("User should not be able to call UnstakeFunction before Time", async () => {
+    try{
+      await stakeInstance.UnStake({from:accounts[2]});
+    } catch (error) {
+      const invalidOpcode = error.message.search("revert") >= 0;
+      console.log(error.message)
+      assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
+    }
+  });
+
+ 
+
   it("Time should increase to 180 days", async () => {
     await time.increase(time.duration.seconds(15638400));
   });
+
+   it("ClaimReward function should work as expected for User2", async () => {
+      const ownerBalanceBefore = await tokenInstance.balanceOf(accounts[0]);
+      const balanceBefore = await tokenInstance.balanceOf(accounts[2]);
+
+      await stakeInstance.ClaimReward({from:accounts[2]});
+      const balanceAfter = await tokenInstance.balanceOf(accounts[2]);
+      const StakeContractBalance = await tokenInstance.balanceOf(stakeInstance.address);
+      
+      const ownerBalanceAfter = await tokenInstance.balanceOf(accounts[0]);
+
+      const totalClaimed = await stakeInstance.totalClaimedRewards();
+      const staker = await stakeInstance.stakers(accounts[2]);
+
+      assert.equal(ownerBalanceBefore.toString(),ownerBalanceAfter.toString(),"Owner has received some unnecessary tax")
+      assert.equal(StakeContractBalance.toString(),"49750000000000000000000","Stake contract Balance before is wrong")
+      assert.equal(balanceBefore.toString(),"37500000000000000000000","Balance before is wrong")
+      assert.equal(balanceAfter.toString(),"62000000000000000000000","Balance after is wrong")
+      assert.equal(totalClaimed.toString(),"27000000000000000000000","Total claimed rewards not correct");
+      assert.equal(staker[1].toString(),"24500000000000000000000","User claimed rewards not correct");
+ 
+  });
+
 
   it("Unstake function should work for USER2", async ()=>{
     const ownerBalanceBefore = await tokenInstance.balanceOf(accounts[0]);
@@ -184,22 +245,34 @@ contract("IFY Staking Contract Test", accounts => {
     const StakeContractBalance = await tokenInstance.balanceOf(stakeInstance.address);
     const ownerBalanceAfter = await tokenInstance.balanceOf(accounts[0]);
 
-    //Checking pending reward for User2
+  //   //Checking pending reward for User2
     const rewardClaimed = await stakeInstance.stakers(accounts[2]);
-    const rewardAfter = await stakeInstance.pendingReward(accounts[2]);
+   // const rewardAfter = await stakeInstance.pendingReward(accounts[2]);
 
-    //CHecking totalStaked State variable and Individual user
+  //   //CHecking totalStaked State variable and Individual user
     const totalStaked = await stakeInstance.totalStaked()
     const staker = await stakeInstance.stakers(accounts[2]);
  
-    assert.equal(rewardClaimed[1].toString(),"0","reward claim is not zero");
-    assert.equal(rewardAfter.toString(),"0","Reward is not assigned correctly")
-   assert.equal(rewardBefore.toString(),"24500000000000000000000","RewardBefre is correct")
+    assert.equal(rewardClaimed[1].toString(),"24500000000000000000000","reward claim is not zero");
+   // assert.equal(rewardAfter.toString(),"0","Reward is not assigned correctly")
+    assert.equal(rewardBefore.toString(),"0","RewardBefre is correct")
+    assert.equal(staker[2].toString(),"0","Pending reward is incorrect")
     assert.equal(ownerBalanceBefore.toString(),ownerBalanceAfter.toString(),"Owner has received some unnecessary tax")
-    assert.equal(StakeContractBalance.toString(),"64250000000000000000000","Stake contract Balance before is wrong")
+    assert.equal(StakeContractBalance.toString(),"39750000000000000000000","Stake contract Balance before is wrong")
     assert.equal(totalStaked.toString(),"10000000000000000000000","Total Stake is not right");
     assert.equal(staker[0].toString(),"0","User balance not updated to ZERO");
 
   })
+
+
+  it("User should not be able to call UnstakeFunction function Twice", async () => {
+    try{
+      await stakeInstance.UnStake({from:accounts[2]});
+    } catch (error) {
+      const invalidOpcode = error.message.search("revert") >= 0;
+      console.log(error.message)
+      assert(invalidOpcode, "Expected revert, got '" + error + "' instead");
+    }
+  });
 
 });
